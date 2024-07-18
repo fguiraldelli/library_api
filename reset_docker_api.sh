d23a6d462f954886a2c483c526d73c8d78cabf7b
Francisco Augusto Guiraldelli <francisco.augusto@clear.sale>
2024-07-18 15:49:18 +0200

    attempt 001

--- reset_docker_api.sh
+++ reset_docker_api.sh
@@ -1,7 +1,7 @@
 docker rm -f $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
-# docker rmi $(docker images -a -q)
+docker rmi $(docker images -a -q)
 docker system prune -f
 docker network create app-network
-# docker-compose build
+docker-compose build
 # docker-compose up -d
